4f3cee682b9f92d03eb2e5264d37e3ae85eed235
qingyuqy <robcqy@gmail.com>
2017-07-21 08:47:57 +0800

    1. New:Personized function for Shot On __ and Photo by __ 2. Adjust: new App name 3. Adjust: Default watermark is oneplus3

--- app/src/main/java/com/qingyu/qcamera/MainActivity.java
+++ app/src/main/java/com/qingyu/qcamera/MainActivity.java
@@ -2,23 +2,41 @@ package com.qingyu.qcamera;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import java.io.File;
@@ -34,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
     private String tempfile;
     private String appPath = "ShotOnOnePlus3";
     private ImageView imageView;
-
+    private static SharedPreferences sp;
+    private String sShot_on;
+    private String sPhote_by;
+    private boolean isPersonized;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            personalized();
             return true;
         }
 
@@ -77,7 +98,13 @@ public class MainActivity extends AppCompatActivity {
             intent.setData(uri);
             this.getApplicationContext().sendBroadcast(intent);
             Bitmap bitmap=BitmapFactory.decodeFile(tempfile);
-            Bitmap waterMark=BitmapFactory.decodeResource(this.getResources(),R.drawable.watermark_oneplus_3);
+            Bitmap waterMark = null;
+            if(!isPersonized){
+                waterMark = BitmapFactory.decodeResource(this.getResources(),R.drawable.watermark_oneplus_3);
+            }else
+            {
+                waterMark = BitmapFactory.decodeResource(this.getResources(),R.drawable.shot);
+            }
             //Log.e("waterMark",waterMark.toString());
             imageView.setImageBitmap(bitmap);
             imageView.setImageBitmap(addWaterMark(this,bitmap,waterMark));
@@ -85,6 +112,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     public void initData(){
+        sp = getSharedPreferences("Shot_On", MODE_PRIVATE);
+        sShot_on = sp.getString("shot_on","OnePlus 3");
+        sPhote_by = sp.getString("photo_by","qingyuqy_");
+        isPersonized = sp.getBoolean("isPersonized",false);
         tempfile = Environment.getExternalStorageDirectory().getPath() + File.separator + appPath + File.separator+ new Date().getTime() + ".jpg";
         Log.e("filePath",tempfile);
     }
@@ -107,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public Bitmap addWaterMark(Context gContext, Bitmap src, Bitmap watermark)
+    public Bitmap addWaterMark(Context context, Bitmap src, Bitmap watermark)
         {
             String tag = "createBitmap";
             Log.d(tag, "create a new bitmap");
@@ -126,6 +157,25 @@ public class MainActivity extends AppCompatActivity {
             cv.drawBitmap(src, 0, 0, null);
             // draw watermark into
             cv.drawBitmap(watermark, 20,h-wh, null);
+
+            if(isPersonized){
+                cv.drawBitmap(watermark, 20,h-wh-20, null);
+                String text = formatTextMark();
+                TextPaint textPaint = new TextPaint();
+                textPaint.setColor(Color.WHITE);
+                textPaint.setTextSize(dp2px(context, 60));
+                textPaint.setAntiAlias(true);
+                StaticLayout layout = new StaticLayout(text, textPaint, w , Layout.Alignment.ALIGN_NORMAL, 1.0F, 0.0F, true);
+                cv.translate(ww+20,h-wh);
+                layout.draw(cv);
+            /*    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint.setColor(Color.WHITE);
+                paint.setTextSize(dp2px(context, 56));
+                Rect bounds = new Rect();
+                paint.getTextBounds(text, 0, text.length(), bounds);*/
+                //cv.drawText(text,ww,h-wh,paint);
+
+            }
             // save all clip
             cv.save(Canvas.ALL_SAVE_FLAG);
             // store
@@ -139,5 +189,93 @@ public class MainActivity extends AppCompatActivity {
             newPic.compress(Bitmap.CompressFormat.JPEG, 100, stream);
             return newPic;
         }
+   public String formatTextMark(){
+       sShot_on = getResources().getString(R.string.shot_on) +" " + sShot_on;
+       sPhote_by = getResources().getString(R.string.photo_by) +" " + sPhote_by;
+       Log.e("sShot_on",sShot_on);
+       Log.e("sPhote_by",sPhote_by);
+       return sShot_on + "\r\n" + sPhote_by;
+   }
+   public void personalized(){
+        final Window win = getWindow();
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View popView = mInflater.inflate(R.layout.personalized, null);
+        final EditText et_shot_on = (EditText) popView.findViewById(R.id.et_shoton);
+        final EditText et_photo_by = (EditText) popView.findViewById(R.id.et_photoby);
+        final CheckBox checkBox = (CheckBox) popView.findViewById(R.id.checkbox);
+        final Button bt_save = (Button) popView.findViewById(R.id.bt_save);
+
+       final PopupWindow popWindow = new PopupWindow(popView,
+               ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+       popWindow.setFocusable(true);
+       popWindow.setBackgroundDrawable(new BitmapDrawable());
+       popWindow.setOutsideTouchable(true);
+       popWindow.showAtLocation(popView, Gravity.CENTER_VERTICAL
+               | Gravity.CENTER_HORIZONTAL, 0, 0);
+       setbackgroundAlpha(0.7f, win);
+       popWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+
+           @Override
+           public void onDismiss() {
+               setbackgroundAlpha(1f, win);
+           }
+       });
+
+        if(isPersonized){
+            checkBox.setChecked(true);
+            et_shot_on.setEnabled(true);
+            et_photo_by.setEnabled(true);
+        }else {
+            checkBox.setChecked(false);
+            et_shot_on.setEnabled(false);
+            et_photo_by.setEnabled(false);
+        }
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    et_shot_on.setEnabled(true);
+                    et_photo_by.setEnabled(true);
+                }else {
+                    et_shot_on.setEnabled(false);
+                    et_photo_by.setEnabled(false);
+                }
+            }
+        });
+
+        sShot_on = sp.getString("shot_on","OnePlus 3");
+        sPhote_by = sp.getString("photo_by","qingyuqy_");
+        et_shot_on.setText(sShot_on);
+        et_photo_by.setText(sPhote_by);
+        bt_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sp.edit();
+                if(!checkBox.isChecked()){
+                    editor.putBoolean("isPersonized",false);
+                }else {
+                editor.putString("shot_on",et_shot_on.getText().toString());
+                editor.putString("photo_by",et_photo_by.getText().toString());
+                    editor.putBoolean("isPersonized",true);}
+                editor.commit();
+                sShot_on = sp.getString("shot_on","OnePlus 3");
+                sPhote_by = sp.getString("photo_by","qingyuqy_");
+                isPersonized = sp.getBoolean("isPersonized",false);
+                Toast.makeText(MainActivity.this, "Save Successfully", Toast.LENGTH_LONG).show();
+                popWindow.dismiss();
+            }
+        });
     }
+
+    public static void setbackgroundAlpha(float bgAlpha, Window win) {
+        WindowManager.LayoutParams lp = win.getAttributes();
+        lp.alpha = bgAlpha; // 0.0-1.0
+        win.setAttributes(lp);
+    }
+
+    public static int dp2px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+}
 
